355d1c865f5426e73f0022ca976eca9671011fa1
BeansFL <106496863+BeansFL@users.noreply.github.com>
2022-11-18 14:58:12 +0100

    Delete client.lua

--- bsc_txannounce/client/client.lua
+++
@@ -1,7 +0,0 @@
-RegisterNetEvent("gh_txAnnouncer:SendRestartMessage", function(timeLeft)
-    SendRestartMessage(timeLeft)
-end)
-
-RegisterNetEvent("gh_txAnnouncer:SendKickedMessage", function(msg)
-    SendKickedMessage(msg)
-end)
