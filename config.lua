c9694fce48074e8e3b14bcfa483fe90ed3a68bbe
BeansFL <106496863+BeansFL@users.noreply.github.com>
2022-11-19 13:54:47 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,9 @@
-ESX = exports['es_extended']:getSharedObject()
-
 
 
 Config = {}
 
-Config.NotificationName = "okokNotify" -- okokNotify, mythic-notify, p-notify, b-notify
-
+Config.NotificationName = "okokNotify" -- okokNotify, mythic-notify, p-notify
+-- Notify
 
 
  
@@ -14,13 +12,13 @@ Config.NotificationName = "okokNotify" -- okokNotify, mythic-notify, p-notify, b
 if Config.NotificationName == "b-notify" then
 function SendRestartMessage(timeLeft)
 
-    TriggerEvent('b-notify:notify', 'server', 'RESTART', 'Der Server wird in ' .. timeLeft .. ' neu gestartet' )
+    TriggerEvent('b-notify:notify', 'server', 'RESTART', 'The server is beeing restarted in ' .. timeLeft .. ' minutes' )
       
 end
 
 function SendKickedMessage(msg)
 
-
+    -- You can comment this out. only for testing purpose
     SetNotificationTextEntry('STRING')
     AddTextComponentString(msg)
     DrawNotification(0,1) 
@@ -32,12 +30,12 @@ else if Config.NotificationName == "okokNotify" then
 
     function SendRestartMessage(timeLeft)
 
-        exports['okokNotify']:Alert('Server', 'Der Server wird in' .. timeLeft .. 'neu gestartet', 5000, 'warning')
+        exports['okokNotify']:Alert('Server', 'The server is beeing restarted in' .. timeLeft .. 'minutes', 5000, 'warning')
     end
     
     function SendKickedMessage(msg)
     
-
+        -- You can comment this out. only for testing purpose
         SetNotificationTextEntry('STRING')
         AddTextComponentString(msg)
         DrawNotification(0,1) 
@@ -48,17 +46,49 @@ else if Config.NotificationName == "okokNotify" then
 else if Config.NotificationName == "mythic-notify" then
     function SendRestartMessage(timeLeft)
 
-        exports['mythic_notify']:SendAlert('Server', 'Der Server wird in' .. timeLeft .. 'neu gestartet!', 2500, { ['background-color'] = '#ffffff', ['color'] = '#000000' })
+        exports['mythic_notify']:SendAlert('Server', 'The server is beeing restarted in' .. timeLeft .. 'minutes!', 2500, { ['background-color'] = '#ffffff', ['color'] = '#000000' })
     end
 
     function SendKickedMessage(msg)
     
-
+        -- You can comment this out. only for testing purpose
         SetNotificationTextEntry('STRING')
         AddTextComponentString(msg)
         DrawNotification(0,1) 
         exports['mythic_notify']:SendAlert('Server', 'Kick', msg,  2500, { ['background-color'] = '#ffffff', ['color'] = '#000000' })
 
     end
+
+else if Config.NotificationName == "p-notify" then
+
+    function SendRestartMessage(timeLeft)
+        TriggerEvent("pNotify:SendNotification", {text = "<span style='font-weight: 900'>" The server is beeing restarted in" .. timeLeft .. "minutes" </span>",
+        layout = "centerLeft",
+        timeout = 2000,
+        progressBar = false,
+        type = "error",
+        animation = {
+            open = "gta_effects_fade_in",
+            close = "gta_effects_fade_out"
+        }})
+end
+    end
+
+    function SendKickedMessage(msg)
+    
+        -- You can comment this out. only for testing purpose
+        SetNotificationTextEntry('STRING')
+        AddTextComponentString(msg)
+        DrawNotification(0,1) 
+        TriggerEvent("pNotify:SendNotification", {text = "<span style='font-weight: 900'>"Spieler gekickt" </span>",
+        layout = "centerLeft",
+        timeout = 2000,
+        progressBar = false,
+        type = "error",
+        animation = {
+            open = "gta_effects_fade_in",
+            close = "gta_effects_fade_out"
+        }})
+end
 
 
